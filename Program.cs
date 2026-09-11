eed41fc15d0893fa80e17bd11fe5c2795d474371
Kaled-Salha <salhakaled@gmail.com>
2026-09-11 09:39:43 +0200

    changed Name

--- Program.cs
+++ Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, Git!");
+        Console.WriteLine(DoSomething("Kaled"));
+    }
+
+    static string DoSomething(string name)
+    {
+        return "Hello {name}...";
     }
 }
